780b9e2ef7f96433dffd123c9cb3c1cb149752b8
JimJam117 <jamessparrow101@gmail.com>
2023-09-23 17:34:59 +0100

    added projectile weapon

--- Assets/player.cs
+++ Assets/player.cs
@@ -9,6 +9,9 @@ public class player : MonoBehaviour
     Rigidbody2D rb;
     public float angle = 0;
 
+    public Rigidbody2D projectile;
+    public bool hasProjectlie = false;
+
     public AnimationClip[] anims;
     public AnimationClip[] stills;
     public AnimationClip[] attacks;
@@ -17,6 +20,21 @@ public class player : MonoBehaviour
 
     public bool isAnim = true;
     public bool playerIsAttacking = false;
+
+
+    public enum AngleDirection
+    {
+        UP = 0,
+        RIGHT_UP = 1,
+        RIGHT = 2,
+        RIGHT_DOWN = 3,
+        DOWN = 4,
+        LEFT_DOWN = 5,
+        LEFT = 6,
+        LEFT_UP = 7
+    }
+
+    public AngleDirection currentAngleDirection = AngleDirection.UP;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +48,7 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        setAngleDirection();
         var PlayerMovement = new Vector2(Input.GetAxis("Horizontal") * 1, Input.GetAxis("Vertical") * 1);
 
         transform.rotation = Quaternion.identity; // keep rotation as initial
@@ -47,12 +66,50 @@ public class player : MonoBehaviour
         if (Input.GetButtonDown("Fire1") && !playerIsAttacking)
         {
             StartCoroutine(attack());
-            var itemsInActiveHitbox = activeHitbox.GetComponent<playerAttackHitbox>().inHitbox;
-            if (itemsInActiveHitbox.Count != 0) {
-                foreach (var item in itemsInActiveHitbox)
+
+            if (hasProjectlie) {
+                Rigidbody2D clone = Instantiate(projectile, transform.position, transform.rotation);
+
+                Vector2 p_velocity = new Vector2(0, 0);
+                switch (currentAngleDirection)
                 {
-                    if (item.GetComponent<damageable>()) {
-                        item.GetComponent<damageable>().damage(10);
+                    case AngleDirection.UP:
+                        p_velocity = new Vector2(0, 2);
+                        break;
+                    case AngleDirection.RIGHT_UP:
+                        p_velocity = new Vector2(1.5f, 1.5f);
+                        break;
+                    case AngleDirection.RIGHT:
+                        p_velocity = new Vector2(2, 0);
+                        break;
+                    case AngleDirection.RIGHT_DOWN:
+                        p_velocity = new Vector2(1.5f, -1.5f);
+                        break;
+                    case AngleDirection.DOWN:
+                        p_velocity = new Vector2(0, -2);
+                        break;
+                    case AngleDirection.LEFT_DOWN:
+                        p_velocity = new Vector2(-1.5f, -1.5f);
+                        break;
+                    case AngleDirection.LEFT:
+                        p_velocity = new Vector2(-2, 0);
+                        break;
+                    case AngleDirection.LEFT_UP:
+                        p_velocity = new Vector2(-1.5f, 1.5f);
+                        break;
+                    default:
+                        break;
+                }
+                clone.velocity = p_velocity;
+            }
+            else {
+                var itemsInActiveHitbox = activeHitbox.GetComponent<playerAttackHitbox>().inHitbox;
+                if (itemsInActiveHitbox.Count != 0) {
+                    foreach (var item in itemsInActiveHitbox)
+                    {
+                        if (item.GetComponent<damageable>()) {
+                            item.GetComponent<damageable>().damage(10);
+                        }
                     }
                 }
             }
@@ -60,50 +117,47 @@ public class player : MonoBehaviour
         }
 
        //Debug.Log("Angle:" + angle);
+    
+            playAnim(isPlayerMoving, ((int)currentAngleDirection)); 
+            setHitbox((int)currentAngleDirection);
+    }
+
+    void setAngleDirection() {
         if (angle >= 247.5f && angle <= 292.5f)
         {
-            playAnim(isPlayerMoving, 0); // up
-            setHitbox(0);
-    }
+            this.currentAngleDirection = AngleDirection.UP;
+        }
         else if (angle < 247.5f && angle >= 202.5f)
         {
-            playAnim(isPlayerMoving, 1); // right up
-            setHitbox(1);
-    }
+            this.currentAngleDirection = AngleDirection.RIGHT_UP;
+        }
         else if (angle < 202.5f && angle >= 157.5f)
         {
-            playAnim(isPlayerMoving, 2); // right
-            setHitbox(2);
-    }
+            this.currentAngleDirection = AngleDirection.RIGHT;
+        }
         else if (angle < 157.5f && angle >= 112.5f)
         {
-            playAnim(isPlayerMoving, 3); // right down
-            setHitbox(3);
-    }
+            this.currentAngleDirection = AngleDirection.RIGHT_DOWN;
+        }
         else if (angle < 112.5f && angle >= 67.5f)
         {
-            playAnim(isPlayerMoving, 4); // down
-            setHitbox(4);
-    }
+            this.currentAngleDirection = AngleDirection.DOWN;
+        }
         else if (angle < 67.5f && angle >= 22.5f)
         {
-            playAnim(isPlayerMoving, 5); // left down
-            setHitbox(5);
-    }
+            this.currentAngleDirection = AngleDirection.LEFT_DOWN;
+        }
 
         else if ((angle < 22.5f) || (angle >= 337.5f))
         {
-            playAnim(isPlayerMoving, 6); // left
-            setHitbox(6);
-    }
-
-        else if (angle < 337.5f && angle >= 292.5f) {
-            playAnim(isPlayerMoving, 7); // left up
-            setHitbox(7);
+            this.currentAngleDirection = AngleDirection.LEFT;
         }
 
+        else if (angle < 337.5f && angle >= 292.5f)
+        {
+            this.currentAngleDirection = AngleDirection.LEFT_UP;
+        }
     }
-
 
     void playAnim(bool isMoving, int id) {
         if (playerIsAttacking)
@@ -125,7 +179,7 @@ public class player : MonoBehaviour
 
     IEnumerator attack() {
         playerIsAttacking = true;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(hasProjectlie ? 1f : 0.1f);
         playerIsAttacking = false;
     }
 }
